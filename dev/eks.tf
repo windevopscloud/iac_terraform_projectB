b73b216f58b93d0d3f38c0cbb49594add8edecdc
windevopscloud <windevopscloud@gmailcom>
2025-08-23 23:50:15 +0400

    first

--- dev/eks.tf
+++ dev/eks.tf
@@ -1,5 +1,5 @@
 module "custom_eks" {
-    source = "git::https://github.com/windevopscloud/iac_terraform_modules.git//eks?ref=v1.0.3"
+    source = "git::https://github.com/windevopscloud/iac_terraform_modules.git//eks?ref=v1.0.4"
     cluster_name = var.cluster_name
     eks_version  = var.eks_version
     region       = var.aws_region
