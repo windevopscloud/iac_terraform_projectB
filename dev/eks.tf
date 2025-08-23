a0c62304933d60e1a36a87c227085e977ebc4efc
windevopscloud <windevopscloud@gmailcom>
2025-08-24 00:16:58 +0400

    first

--- dev/eks.tf
+++ dev/eks.tf
@@ -2,8 +2,8 @@ module "custom_eks" {
     source = "git::https://github.com/windevopscloud/iac_terraform_modules.git//eks?ref=v1.0.8"
     cluster_name = var.cluster_name
     eks_version  = var.eks_version
-    region       = var.aws_region
+    aws_region   = var.aws_region
     private_subnets = module.vpc.private_subnets
-    scaling_type = var.scaling_type
-    node_group = var.node_group
+    scaling_type    = var.scaling_type
+    node_group      = var.node_group
 }
