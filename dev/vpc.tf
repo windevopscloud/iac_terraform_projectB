7d431f40b02bc0419991464ab5098026a51b38a5
windevopscloud <windevopscloud@gmailcom>
2025-08-24 10:14:41 +0400

    first

--- dev/vpc.tf
+++ dev/vpc.tf
@@ -1,6 +1,6 @@
 module "vpc" {
   #source = "./vpc-module"  # Path to your module
-  source = "git::https://github.com/windevopscloud/iac_terraform_modules.git//vpc?ref=v1.0.12"
+  source = "git::https://github.com/windevopscloud/iac_terraform_modules.git//vpc?ref=v1.0.21"
 
   vpc_cidr = var.vpc_cidr
   azs      = var.azs
