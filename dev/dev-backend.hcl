76bab7ddb13b112039e6de32aab1e1bf165eeae5
windevopscloud <156822361+windevopscloud@users.noreply.github.com>
2025-08-25 18:41:33 +0400

    Delete dev directory

--- dev/dev-backend.hcl
+++
@@ -1,5 +0,0 @@
-bucket         = "windevopscloud-terraform-s3"
-key            = "projectB-dev/terraform.tfstate"
-region         = "us-east-1"
-encrypt        = true
-use_lockfile   = true
--- dev/eks.tf
+++
@@ -1,9 +0,0 @@
-module "custom_eks" {
-    source = "git::https://github.com/windevopscloud/iac_terraform_modules.git//eks?ref=v1.0.3"
-    cluster_name = var.cluster_name
-    eks_version  = var.eks_version
-    region       = var.aws_region
-    private_subnets = module.vpc.private_subnets
-    scaling_type = var.scaling_type
-    node_group = var.node_group
-}
